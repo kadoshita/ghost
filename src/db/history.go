6d55106313a8d4d703bcbaacb47dea050f513833
kadoshita <sublimer.me@gmail.com>
2020-11-20 22:05:56 +0900

    add history set and get function

--- src/db/history.go
+++ src/db/history.go
@@ -1,6 +1,10 @@
 package db
 
-import "github.com/jinzhu/gorm"
+import (
+	"log"
+
+	"github.com/jinzhu/gorm"
+)
 
 // History はHostの死活ステータスの履歴を持つ
 type History struct {
@@ -8,4 +12,22 @@ type History struct {
 	HostInfoID uint `json:"host"`
 	HostInfo   HostInfo
 	IsUP       bool `json:"isup"`
+}
+
+func GetHistories(limit int) (histories []History) {
+	db := getDB()
+	if limit < 1 {
+		log.Println("limit must be greater than or equal to 1.")
+		return
+	}
+	db.Limit(limit).Find(&histories)
+	return
+}
+
+func InsertHistory(history History) error {
+	db := getDB()
+	if db.NewRecord(history) {
+		return db.Create(&history).Error
+	}
+	return nil
 }
